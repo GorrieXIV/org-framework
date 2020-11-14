462a3aba8ec65ce85f71f5cbdb52fe8ac9bf779f
GorrieXIV <rob@arkelltech.com>
2020-11-14 01:26:45 -0500

    Adding more buttons and prepariing for sprite identification functionality in sprite sheet builder.

--- tools/sprite_sheet_builder.py
+++ tools/sprite_sheet_builder.py
@@ -6,7 +6,7 @@ from PyQt5.QtWidgets import (
     QTextEdit,
     QPushButton,
     QLabel,
-    QVBoxLayout,
+    QGridLayout,
 )
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import QDir
@@ -20,28 +20,40 @@ class DialogApp(QWidget):
         self.resize(800, 600)
         self.setWindowTitle("ORG Engine Sprite Sheet Builder")
 
-        self.button1 = QPushButton("Open Image File")
-        self.button1.clicked.connect(self.get_image_file)
+        self.openImageButton = QPushButton("Open Image File")
+        self.openImageButton.clicked.connect(self.getImageFile)
 
-        self.button2 = QPushButton("Manually Edit Sprite Sheet JSON")
-        self.button2.clicked.connect(self.get_text_file)
+        self.selectionButton = QPushButton("Selection Tool")
+        self.selectionButton.clicked.connect(self.activateRectangleSelection)
+
+        self.gridButton = QPushButton("Grid Tool")
+        self.gridButton.clicked.connect(self.openGridToolDialog)
+
+        self.openJsonButton = QPushButton("Manually Edit Sprite Sheet JSON")
+        self.openJsonButton.clicked.connect(self.openJsonFile)
 
         self.labelImage = QLabel()
         self.textEditor = QTextEdit()
 
-        layout = QVBoxLayout()
-        layout.addWidget(self.button1)
-        layout.addWidget(self.labelImage)
-        layout.addWidget(self.button2)
-        layout.addWidget(self.textEditor)
+        layout = QGridLayout()
+
+        # Add buttons to layout.
+        layout.addWidget(self.openImageButton, 0, 0)
+        layout.addWidget(self.selectionButton, 1, 0)
+        layout.addWidget(self.gridButton, 2, 0)
+        layout.addWidget(self.openJsonButton, 3, 0)
+
+        # Add content labels to layout.
+        layout.addWidget(self.labelImage, 0, 1)
+        layout.addWidget(self.textEditor, 4, 1)
 
         self.setLayout(layout)
 
-    def get_image_file(self):
+    def getImageFile(self):
         file_name, _ = QFileDialog.getOpenFileName(self, "Open Image File", "./", "Image files (*.jpg *.jpeg *.png *.gif)")
         self.labelImage.setPixmap(QPixmap(file_name))
 
-    def get_text_file(self):
+    def openJsonFile(self):
         dialog = QFileDialog()
         dialog.setFileMode(QFileDialog.AnyFile)
         dialog.setFilter(QDir.Files)
@@ -53,6 +65,12 @@ class DialogApp(QWidget):
                     data = f.read()
                     self.textEditor.setPlainText(data)
                     f.close()
+
+    def activateRectangleSelection(self):
+        pass
+
+    def openGridToolDialog(self):
+        pass
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
