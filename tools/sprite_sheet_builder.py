4db4ffd9c3a22e83ed15c6855ff8189984d60af5
GorrieXIV <rob@arkelltech.com>
2020-11-15 16:24:29 -0500

    adding grid tool dialog to sprite sheet builder.

--- tools/sprite_sheet_builder.py
+++ tools/sprite_sheet_builder.py
@@ -7,12 +7,23 @@ from PyQt5.QtWidgets import (
     QPushButton,
     QLabel,
     QGridLayout,
+    QFormLayout,
+    QDialog,
+    QDialogButtonBox,
+    QLineEdit,
 )
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtCore import QDir
+from PyQt5.QtGui import (
+    QPixmap,
+    QBrush,
+    QPainter,
+    QColor
+)
+from PyQt5.QtCore import QDir, QRect
+
+import simplejson as json
 import sys
 
-class DialogApp(QWidget):
+class SheetBuilderMainWindow(QWidget):
     def __init__(self):
 
         # Set window dimensions and title.
@@ -29,7 +40,7 @@ class DialogApp(QWidget):
         self.gridButton = QPushButton("Grid Tool")
         self.gridButton.clicked.connect(self.openGridToolDialog)
 
-        self.openJsonButton = QPushButton("Manually Edit Sprite Sheet JSON")
+        self.openJsonButton = QPushButton("Edit Sprite Sheet JSON")
         self.openJsonButton.clicked.connect(self.openJsonFile)
 
         self.labelImage = QLabel()
@@ -39,13 +50,13 @@ class DialogApp(QWidget):
 
         # Add buttons to layout.
         layout.addWidget(self.openImageButton, 0, 0)
-        layout.addWidget(self.selectionButton, 1, 0)
-        layout.addWidget(self.gridButton, 2, 0)
-        layout.addWidget(self.openJsonButton, 3, 0)
+        layout.addWidget(self.selectionButton, 0, 1)
+        layout.addWidget(self.gridButton, 0, 2)
+        layout.addWidget(self.openJsonButton, 0, 3)
 
         # Add content labels to layout.
-        layout.addWidget(self.labelImage, 0, 1)
-        layout.addWidget(self.textEditor, 4, 1)
+        layout.addWidget(self.labelImage, 1, 0, 1, 4)
+        layout.addWidget(self.textEditor, 2, 0, 4, 4)
 
         self.setLayout(layout)
 
@@ -70,10 +81,50 @@ class DialogApp(QWidget):
         pass
 
     def openGridToolDialog(self):
-        pass
+        dialog = GridToolDialogBox()
+        if dialog.exec():
+            width, height, columns, rows = dialog.getInputs()
+            painter = QPainter(self)
+            brush = QBrush(QColor(100, 10, 10, 40))
+            painter.setBrush(brush)
+            try:
+                painter.drawRect(int(0), int(0), int(width), int(height))
+            except ValueError as e:
+                print("Invalid inputs given for grid dimensions.")
+
+class GridToolDialogBox(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.spriteWidth = QLineEdit(self)
+        self.spriteHeight = QLineEdit(self)
+        self.columns = QLineEdit(self)
+        self.rows = QLineEdit(self)
+        formSubmitBox = QDialogButtonBox(
+            QDialogButtonBox.Ok | QDialogButtonBox.Cancel,
+            self
+        )
+
+        layout = QFormLayout(self)
+        layout.addRow("Sprite Width", self.spriteWidth)
+        layout.addRow("Sprite Height", self.spriteHeight)
+        layout.addRow("Number of Columns", self.columns)
+        layout.addRow("Number of Rows", self.rows)
+        layout.addWidget(formSubmitBox)
+
+        formSubmitBox.accepted.connect(self.accept)
+        formSubmitBox.rejected.connect(self.reject)
+
+    def getInputs(self):
+        return (
+            self.spriteWidth.text(),
+            self.spriteHeight.text(),
+            self.columns.text(),
+            self.rows.text()
+        )
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    demo = DialogApp()
+    demo = SheetBuilderMainWindow()
     demo.show()
     sys.exit(app.exec_())
